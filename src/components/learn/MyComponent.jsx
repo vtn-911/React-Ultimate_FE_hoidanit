a38f251cff423a276477be24b26e6650c765b894
Tuyet Nhi <vutuyetnhi0911@gmail.com>
2025-06-28 08:21:47 +0700

    #31 Cach su dung bien voi JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,10 +1,17 @@
-//JSX
+// 28/6
+// Cách sử dụng biến số với JSX
 import "./style.css";
 const MyComponent = () => {
+  // const  hoidanit = 25;
+  // const  hoidanit = [1,2,3];
+  const  hoidanit = {
+    name: "hoidanit",
+    age: 25
+  };
   return (
-    //Fragment: giúp không render thừa thải HTML
     <>
-        <div>eric & hoidanit</div>
+        <div> {JSON.stringify(hoidanit)} & hoidanit</div>
+        <div>{console.log('ERIC')}</div>
         <div className="child">child</div>
     </>
   );
