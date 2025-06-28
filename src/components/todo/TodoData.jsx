992b35f4b06e49776c60084c245ceb9bcd0a87b3
Tuyet Nhi <vutuyetnhi0911@gmail.com>
2025-06-28 09:55:57 +0700

    #33 Components Exercise

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,8 @@
+const TodoData = () => {
+    return (
+        <div className='todo-data'>
+        <div>Learning React</div>
+      </div>
+    )
+}
+export default TodoData;
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,9 @@
+const TodoNew = () =>{
+    return (
+        <div className='todo-new'>
+        <input type="text" className='todo-input' placeholder='Enter your task' />
+        <button className='todo-button'>Add</button>
+      </div>
+    )
+}
+export default TodoNew;
