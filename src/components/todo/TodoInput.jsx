b9010a721f051a4ede8867b8eeb84dbafa9bfbf3
Tuyet Nhi <vutuyetnhi0911@gmail.com>
2025-07-01 09:48:51 +0700

    #36 DOM Events

--- src/components/todo/TodoInput.jsx
+++ src/components/todo/TodoInput.jsx
@@ -1,8 +1,11 @@
 const TodoInput = (props) =>{
     const {addNewTodo} = props
+    const handleOnchange = (name) => {
+      console.log('check handleonchange',name)
+    }
     return (
         <div className='todo-new'>
-        <input type="text" className='todo-input' placeholder='Enter your task' />
+        <input type="text" className='todo-input' placeholder='Enter your task' onChange={(event) => handleOnchange(event.target.value)} />
         <button className='todo-button' onClick={addNewTodo}>Add</button>
       </div>
     )
