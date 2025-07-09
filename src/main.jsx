edaea5e18007b96d4982f0577fa40aebb3722d93
Tuyet Nhi <vutuyetnhi0911@gmail.com>
2025-07-09 08:59:55 +0700

    #45 Tích hợp Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+// Tích hợp Router & Cách tạo đường link dẫn đến trang khác
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/register",
+    element: <div>Register page</div>
+  },
+  {
+    path: "/login",
+    element: <div>Login page</div>
+  },
+  {
+    path: "/users",
+    element: <div>Users page</div>
+  }
+]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
