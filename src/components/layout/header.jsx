53b2d1c4eac27980866470f70147dceabf71089c
Tuyet Nhi <vutuyetnhi0911@gmail.com>
2025-07-13 21:11:41 +0700

    #49 Client Route với Link

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
 import "./header.css"
 const Header = () => {
     return (
         <ul>
-            <li><a className="active" href="/">Home</a></li>
-            <li><a href="/users">Users</a></li>
-            <li><a href="/products">Products</a></li>
+            {/* thay đổi thẻ a -> Link : giúp trang không bị reload lại */}
+            <li><Link className="active" to="/">Home</Link></li>
+            <li><Link to="/users">Users</Link></li>
+            <li><Link to="/products">Products</Link></li>
         </ul>
 
     )
